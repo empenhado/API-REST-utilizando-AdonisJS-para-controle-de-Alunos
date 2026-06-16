9884faede0a4fb870215c3bf0fe3d811add4e343
empenhado <vitalb@acad.ifma.edu.br>
2026-06-16 17:48:25 -0300

    docs: inicio da documentaçao

--- controle-alunos-api/start/routes.ts
+++ controle-alunos-api/start/routes.ts
@@ -6,10 +6,26 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
-import { middleware } from '#start/kernel'
+// start/routes.ts
 import router from '@adonisjs/core/services/router'
+import swaggerConfig from '#config/swagger'
+import AutoSwagger from 'adonis-autoswagger'
+import { middleware } from '#start/kernel'
 import { controllers } from '#generated/controllers'
+
+// Endpoint que gera o arquivo de especificação OpenAPI (YAML)
+router.get('/swagger', async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swaggerConfig)
+})
+
+// Endpoint que renderiza a interface visual para testes
+router.get('/docs', async () => {
+  return AutoSwagger.default.ui('/swagger', swaggerConfig)
+
+  // Dica: Se preferir uma interface mais moderna que o Swagger clássico,
+  // você pode trocar a linha acima por:
+  // return AutoSwagger.default.scalar('/swagger')
+})
 
 router.get('/', () => {
   return { hello: 'world' }
